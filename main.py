4256ac485e7fd0b2e0dad2158ec3a1b3b978e0cc
Ayar Maw <ayarlwinmaw@gmail.com>
2025-10-15 23:05:39 -0700

    update webapp

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import streamlit as st
+from streamlit import set_page_config
+
 import functions
 
 todos = functions.get_todos()
 
+set_page_config(
+    title = "Todos",
+    layout = "vertical"
+)
 def add_todo():
     todo = st.session_state['new_todo'] + "\n"
     todos.append(todo)
@@ -10,7 +16,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todoapp")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="New todo:", placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,5 +30,4 @@ for index, todo in enumerate(todos):
 
 
 
-st.text_input(label="New todo:", placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
+st.session_state
